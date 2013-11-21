fe2afc0dc3c8e220bdc08aaf1490034f894be538
Wisdom Wolf <WisdomWolf@gmail.com>
2013-11-20 21:40:50 -0500

    initial commit

---
+++ src/com/example/notificationtester/AccessService.java
@@ -0,0 +1,134 @@
+package com.example.notificationtester;
+
+import android.accessibilityservice.AccessibilityService;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+public class AccessService extends AccessibilityService {
+	public void onAccessibilityEvent(AccessibilityEvent event) {
+		PackageManager pm = getPackageManager();
+		String eventPackageName;
+        if (event.getPackageName() != null){
+            eventPackageName = event.getPackageName().toString();
+        } else {
+            eventPackageName = "";
+        }
+        // get the title
+        String title = "";
+        try {
+            title = pm.getApplicationLabel(pm.getApplicationInfo(eventPackageName, 0)).toString();
+        } catch (NameNotFoundException e) {
+            title = eventPackageName;
+        }
+        // get the notification text
+        String notificationText = event.getText().toString();
+        // strip the first and last characters which are [ and ]
+        notificationText = notificationText.substring(1, notificationText.length() - 1);
+        
+        Parcelable parcelable = event.getParcelableData();
+        if (parcelable instanceof Notification) {
+        	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+        		notificationText += "\n" + getExtraBigData((Notification) parcelable, notificationText.trim());
+            } else {
+                notificationText += "\n" + getExtraData((Notification) parcelable, notificationText.trim());
+            }
+
+        }
+        Intent i = new  Intent("com.example.notificationlistener.NOTIFICATION_LISTENER_EXAMPLE");
+        i.putExtra("notification_event", notificationText);
+        sendBroadcast(i);
+            
+	}
+	
+    @Override
+    public void onInterrupt() {
+
+    }
+	private String getExtraData(Notification notification, String existing_text) {
+        RemoteViews views = notification.contentView;
+        if (views == null) {
+            return "";
+        }
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        try {
+            ViewGroup localView = (ViewGroup) inflater.inflate(views.getLayoutId(), null);
+            views.reapply(getApplicationContext(), localView);
+            return dumpViewGroup(0, localView, existing_text);
+        } catch (android.content.res.Resources.NotFoundException e) {
+            return "";
+        } catch (RemoteViews.ActionException e) {
+            return "";
+        }
+    }
+    
+    @TargetApi(18)
+    private String getExtraBigData(Notification notification, String existing_text) {
+        RemoteViews views = null;
+        try {
+            views = notification.bigContentView;
+        } catch (NoSuchFieldError e) {
+            return getExtraData(notification, existing_text);
+        }
+        if (views == null) {
+            return getExtraData(notification, existing_text);
+        }
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        try {
+            ViewGroup localView = (ViewGroup) inflater.inflate(views.getLayoutId(), null);
+            views.reapply(getApplicationContext(), localView);
+            return dumpViewGroup(0, localView, existing_text);
+        } catch (android.content.res.Resources.NotFoundException e) {
+            return "";
+        }
+    }
+    
+    private String dumpViewGroup(int depth, ViewGroup vg, String existing_text) {
+        String text = "";
+        for (int i = 0; i < vg.getChildCount(); ++i) {
+            View v = vg.getChildAt(i);
+            if (v.getId() == android.R.id.title || v instanceof android.widget.Button
+                    || v.getClass().toString().contains("android.widget.DateTimeView")) {
+                if (existing_text.isEmpty() && v.getId() == android.R.id.title) {
+                } else {
+                    continue;
+                }
+            }
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                if (tv.getText().toString() == "..."
+                        || isInteger(tv.getText().toString())
+                        || tv.getText().toString().trim().equalsIgnoreCase(existing_text)) {
+                    continue;
+                }
+                text += tv.getText().toString() + "\n";
+            }
+            if (v instanceof ViewGroup) {
+                text += dumpViewGroup(depth + 1, (ViewGroup) v, existing_text);
+            }
+        }
+        return text;
+    }
+    
+    public boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/com/example/notificationtester/NLService.java
@@ -0,0 +1,173 @@
+package com.example.notificationtester;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Parcelable;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+@TargetApi(18)
+public class NLService extends NotificationListenerService {
+
+    private String TAG = this.getClass().getSimpleName();
+    private NLServiceReceiver nlservicereciver;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        nlservicereciver = new NLServiceReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.example.notificationlistener.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+        registerReceiver(nlservicereciver,filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nlservicereciver);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+        Log.i(TAG,"**********  onNotificationPosted");
+        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
+        Log.i(TAG,"Notification String:" + sbn.toString());
+        Log.i(TAG,"Intent:" + " " + sbn.getNotification().contentIntent.toString());
+        Intent i = new  Intent("com.example.notificationlistener.NOTIFICATION_LISTENER_EXAMPLE");
+        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "\n");
+        sendBroadcast(i);
+        notificationCapture(sbn);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        Log.i(TAG,"********** onNotificationRemoved");
+        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText +"\t" + sbn.getPackageName());
+        Intent i = new  Intent("com.example.notificationlistener.NOTIFICATION_LISTENER_EXAMPLE");
+        i.putExtra("notification_event","onNotificationRemoved :" + sbn.getPackageName() + "\n");
+        sendBroadcast(i);
+    }
+    
+    public void notificationCapture(StatusBarNotification sbn){
+    	Log.i(TAG,"********* notificationCapture");
+    	Intent i = new  Intent("com.example.notificationlistener.NOTIFICATION_LISTENER_EXAMPLE");
+    	String notificationText = "";
+    	Parcelable parcelable = sbn.getNotification();
+    	notificationText += "\n" + getExtraBigData((Notification) parcelable, notificationText.trim());
+    	i.putExtra("notification_event"," " + sbn.getPackageName() + "\n" 
+    			+ notificationText + "\n");
+    	sendBroadcast(i);
+   }
+    private String getExtraData(Notification notification, String existing_text) {
+    	Log.i(TAG,"******** getExtraData");
+        RemoteViews views = notification.contentView;
+        if (views == null) {
+            return "";
+        }
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        try {
+            ViewGroup localView = (ViewGroup) inflater.inflate(views.getLayoutId(), null);
+            views.reapply(getApplicationContext(), localView);
+            return dumpViewGroup(0, localView, existing_text);
+        } catch (android.content.res.Resources.NotFoundException e) {
+            return "";
+        } catch (RemoteViews.ActionException e) {
+            return "";
+        }
+    }
+    
+    @TargetApi(18)
+    private String getExtraBigData(Notification notification, String existing_text) {
+    	Log.i(TAG,"******** getExtraBigData");
+        RemoteViews views = null;
+        try {
+            views = notification.bigContentView;
+        } catch (NoSuchFieldError e) {
+            return getExtraData(notification, existing_text);
+        }
+        if (views == null) {
+            return getExtraData(notification, existing_text);
+        }
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        try {
+            ViewGroup localView = (ViewGroup) inflater.inflate(views.getLayoutId(), null);
+            views.reapply(getApplicationContext(), localView);
+            return dumpViewGroup(0, localView, existing_text);
+        } catch (android.content.res.Resources.NotFoundException e) {
+            return "";
+        }
+    }
+    
+    private String dumpViewGroup(int depth, ViewGroup vg, String existing_text) {
+        String text = "";
+        for (int i = 0; i < vg.getChildCount(); ++i) {
+            View v = vg.getChildAt(i);
+            if (v.getId() == android.R.id.title || v instanceof android.widget.Button
+                    || v.getClass().toString().contains("android.widget.DateTimeView")) {
+                if (existing_text.isEmpty() && v.getId() == android.R.id.title) {
+                } else {
+                    continue;
+                }
+            }
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                if (tv.getText().toString() == "..."
+                        || isInteger(tv.getText().toString())
+                        || tv.getText().toString().trim().equalsIgnoreCase(existing_text)) {
+                    continue;
+                }
+                text += tv.getText().toString() + "\n";
+            }
+            if (v instanceof ViewGroup) {
+                text += dumpViewGroup(depth + 1, (ViewGroup) v, existing_text);
+            }
+        }
+        return text;
+    }
+    
+    public boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+
+    class NLServiceReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getStringExtra("command").equals("clearall")){
+                    NLService.this.cancelAllNotifications();
+            }
+            else if(intent.getStringExtra("command").equals("list")){
+                Intent i1 = new  Intent("com.example.notificationlistener.NOTIFICATION_LISTENER_EXAMPLE");
+                i1.putExtra("notification_event","=====================");
+                sendBroadcast(i1);
+                for (StatusBarNotification sbn : NLService.this.getActiveNotifications()) {
+                   notificationCapture(sbn);
+                }
+                Intent i2 = new  Intent("com.example.notificationlistener.NOTIFICATION_LISTENER_EXAMPLE");
+                i2.putExtra("notification_event","===== Notification List ====");
+                sendBroadcast(i2);
+
+            }
+
+        }
+    }
+
+}
